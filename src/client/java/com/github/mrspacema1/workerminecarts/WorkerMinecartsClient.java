0ee4ba99519e885e7421bed0e931f5795e003668
MrSpaceMan1 <bartek99q@gmail.com>
2024-07-30 17:24:48 +0200

    Inital commit

---
+++ src/client/java/com/github/mrspacema1/workerminecarts/WorkerMinecartsClient.java
@@ -0,0 +1,10 @@
+package com.github.mrspacema1.workerminecarts;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WorkerMinecartsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
